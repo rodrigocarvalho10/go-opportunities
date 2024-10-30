7066b737e103228b84d94a3a19302752bb6de578
rodrigocarvalho10 <rodrigo.carvalho.it@gmail.com>
2024-10-30 18:40:19 -0300

    Subindo versão nova do router

--- router/router.go
+++ router/router.go
@@ -1,5 +1,20 @@
 package router
 
+import (
+	"github.com/gin-gonic/gin"
+
+)
+
 func Initialize() {
 
+	router *gin.Engine := gin.Default()
+
+	router.GET(relativePath: "/ping", handlers ... : func (c *gin.Context)  {
+		c.JSON(code: 200, obj: gin.H{
+			"message": "pong",
+		})
+		
+	})
+
+	router.Run(addr ... : ":8080")
 }
